bd9e8ff8801feec104dfd783e83f7f3d27d4387f
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-01-14 20:59:48 +0530

    Add files via upload
    Java Program to Check if a Given Integer is Odd or Even (Using Brute Force)

---
+++ Even_or_Odd.java
@@ -0,0 +1,21 @@
+// Java Program to Check if Given Integer is Odd or Even
+
+import java.io.*;
+import java.util.Scanner;
+
+class Even_or_Odd {
+
+    public static void main(String[] args)
+    {
+        int num = 10;
+        if (num % 2 == 0) {
+
+            System.out.println("Entered Number is Even");
+        }
+
+        else {
+            
+            System.out.println("Entered Number is Odd");
+        }
+    }
+}
