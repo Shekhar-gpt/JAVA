8538fbfa8a3a2cee48c60ee67db263a5f29481ee
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-01-19 12:01:14 +0530

    Left Triangle Star Pattern
    Java Program to Print Left Triangle Star Pattern

---
+++ LeftTriangle.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LeftTriangle {
+    public static void main(String[] args)
+    {
+        int n = 5;
+
+        IntStream.range(1, n + 1).forEach(row -> {
+            String stars
+                = IntStream.range(1, row + 1)
+                      .mapToObj(i -> "* ")
+                      .collect(Collectors.joining());
+            System.out.printf("%" + n * 2 + "s%n", stars);
+        });
+    }
+}
