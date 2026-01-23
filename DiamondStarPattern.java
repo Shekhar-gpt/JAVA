4fe747c420915b59fdd58d38fc224ea1bc245886
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-01-23 21:01:42 +0530

    Add files via upload
    Java Program to Print Diamond Shape Star Pattern (using for loop)

---
+++ DiamondStarPattern.java
@@ -0,0 +1,38 @@
+// Java program to print diamond star pattern Using for loop
+
+import java.io.*;
+
+public class DiamondStarPattern {
+    public static void main(String[] args)
+    {
+
+        int number = 7;
+
+        int m, n;
+
+        for (m = 1; m <= number; m++) {
+
+            for (n = 1; n <= number - m; n++) {
+                System.out.print(" ");
+            }
+
+            for (n = 1; n <= m * 2 - 1; n++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for (m = number - 1; m > 0; m--) {
+
+            for (n = 1; n <= number - m; n++) {
+                System.out.print(" ");
+            }
+
+            for (n = 1; n <= m * 2 - 1; n++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
