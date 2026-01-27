ca8b4a34bfbea05d4160dae7abc7b0ef489e974f
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-01-27 23:13:49 +0530

    Add files via upload

---
+++ DecimalToBinary.java
@@ -0,0 +1,23 @@
+public class DecimalToBinary {
+
+    public static void main(String[] args)
+    {
+        int decimal = 10;
+        String binary = decimalToBinary(decimal);
+        System.out.println("Decimal: " + decimal);
+        System.out.println("Binary: " + binary);
+    }
+
+    public static String decimalToBinary(int n)
+    {
+        int remainder, quotient = n;
+        String binaryNum = "";
+        while (quotient > 0) {
+            remainder = quotient % 2;
+            binaryNum
+                = Integer.toString(remainder) + binaryNum;
+            quotient = quotient / 2;
+        }
+        return binaryNum;
+    }
+}
