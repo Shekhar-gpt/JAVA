b745846983cc5bf5391d6f01a67eed92c0fc65e9
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-01-27 23:09:39 +0530

    Add files via upload

---
+++ DecimalToHexadecimal.java
@@ -0,0 +1,11 @@
+// To convert Decimal to Hexadecimal Number
+import java.util.*;
+
+public class DecimalToHexadecimal {
+    public static void main(String[] args)
+    {
+        int number = 2545;
+
+        System.out.println(Integer.toString(number, 16));
+    }
+}
