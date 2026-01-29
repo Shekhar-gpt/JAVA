c3497c45d46efe5a672858ff3f3f8d5b1a5e1cd0
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-01-29 15:07:12 +0530

    Add files via upload

---
+++ IntegerToChar.java
@@ -0,0 +1,16 @@
+// Java Program to Illustrate Integer to Char Conversion
+
+import java.util.*;
+
+class IntegerToChar
+{
+
+    public static void main(String[] args)
+    {
+        int i = 97;
+
+        char ch = (char)i;
+
+        System.out.println(ch);
+    }
+}
