f6b7f15b43e57a9e772dcbd2d15c33ce238c4109
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-01-30 16:01:53 +0530

    Add files via upload

---
+++ SoftwarePrice.java
@@ -0,0 +1,20 @@
+public class SoftwarePrice {
+
+    static String name;
+    static float price;
+
+    static void set(String n, float p) {
+        name = n;
+        price = p;
+    }
+
+    static void get() {
+        System.out.println("Software name is: " + name);
+        System.out.println("Software price is: " + price);
+    }
+
+    public static void main(String[] args) {
+        SoftwarePrice.set("Visual Studio", 0.0f);
+        SoftwarePrice.get();
+    }
+}
